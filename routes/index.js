b40a480e15e337978c25bd9e28c29496a42f0eae
Ryan Stanley <stanleyrya@gmail.com>
2015-12-22 23:21:57 -0500

    our first heart-beat!

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var graph = require('../lib/graph')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  graph.postStatus('First post in a long time!\nLove Larry :^)');
+
   res.render('index', { title: 'Express' });
 });
 
